aa8e6000280bec91ecf8b46b07fc20aabd7c0b2b
PIV-y <iv.proshin@yandex.ru>
2023-09-19 10:32:50 +0400

    com1

--- src/main/java/ru/kata/spring/boot_security/demo/model/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -31,6 +31,11 @@ public class User implements UserDetails {
     @Column(name = "lastname")
     private String lastName;
 
+    @NotEmpty(message = "Email не может быть пустым")
+    @Size(min = 6, max = 100, message = "Email должен быть длиной от 2 до 100 символов")
+    @Column(name = "email_address")
+    private String email;
+
     @Min(value = 10, message = "Вам не может быть меньше 10 лет")
     @Column(name = "age")
     private Integer age;
@@ -48,9 +53,10 @@ public class User implements UserDetails {
 
     public User() {}
 
-    public User(String username, String lastName, Integer age, String password, List<Role> roles) {
+    public User(String username, String lastName, String email, Integer age, String password, List<Role> roles) {
         this.username = username;
         this.lastName = lastName;
+        this.email = email;
         this.age = age;
         this.password = password;
         this.roles = roles;
@@ -65,28 +71,16 @@ public class User implements UserDetails {
         this.id = id;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getUsername() {
         return username;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return false;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return false;
     }
 
     public void setUsername(String username) {
@@ -107,6 +101,26 @@ public class User implements UserDetails {
 
     public void setAge(Integer age) {
         this.age = age;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
     }
 
     @Override
@@ -134,16 +148,16 @@ public class User implements UserDetails {
         this.roles.add(role);
     }
 
-
     @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
                 ", username='" + username + '\'' +
                 ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
                 ", age=" + age +
+                ", password='" + password + '\'' +
                 ", roles=" + roles +
                 '}';
     }
-
 }
